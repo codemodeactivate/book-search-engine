d9da4c51bf23a88c2d4fd1f69c9fad7179bcb771
codemodeactivate <jeff.zenko@gmail.com>
2023-07-20 18:09:37 -0400

    server.js refactored

--- server/server.js
+++ server/server.js
@@ -2,19 +2,40 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
 
-const app = express();
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.start().then(() => {
+  server.applyMiddleware({ app });
+});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// wildcard GET route for serving the React App
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
